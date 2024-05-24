9eb593fa319abcec718e5c7ba394f4b56101493f
Dprincecoder <78590038+dprincecoder@users.noreply.github.com>
2024-05-24 12:43:43 +0100

    Update firebase-config.js

--- firebase-app/src/firebase-config.js
+++ firebase-app/src/firebase-config.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyDvbVZQDkbVWbdelQam7lfGCFV0r_tDjd4',
-	authDomain: 'fir-auth-9b91e.firebaseapp.com',
-	projectId: 'fir-auth-9b91e',
-	storageBucket: 'fir-auth-9b91e.appspot.com',
-	messagingSenderId: '132948710041',
-	appId: '1:132948710041:web:a84e734a149b2ecf3dde08',
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
 };
 
 // Initialize Firebase
